b16a8ecb83217a71ce038a1ba160c2d1df2b265b
Chad Sundberg <chadsundberg@gmail.com>
2017-05-31 16:14:58 -0500

    first commit

---
+++ ch02/02_04/comments.php
@@ -0,0 +1,17 @@
+<?php
+
+//this is a comment
+#this is also a comment in php
+
+$firstName = 'David';  // this variable stores the presenter's first name
+
+$lastName = 'Powers';  # this variable stores the presenter's last name
+
+/* This is a multiple line comment.
+    And it finishes here and uses same syntax and CSS comments. */
+
+/* echo $firstName;
+echo ' '; */
+echo $lastName;
+
+
